0695a6e590173a61a1e1fc517ab2bf4636c9f271
Arctic Ice Studio <development@arcticicestudio.com>
2019-05-25 17:52:23 +0200

    Update outdated dependencies
    Also updated the doc comments and depencency configs.
    
    GH-127

--- .remarkrc.js
+++ .remarkrc.js
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2017-present Arctic Ice Studio <development@arcticicestudio.com>
- * Copyright (c) 2017-present Sven Greb <development@svengreb.de>
+ * Copyright (C) 2017-present Arctic Ice Studio <development@arcticicestudio.com>
+ * Copyright (C) 2017-present Sven Greb <development@svengreb.de>
  *
  * Project:    Nord Visual Studio Code
  * Repository: https://github.com/arcticicestudio/nord-visual-studio-code
--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,10 @@
 /*
- * Copyright (c) 2017-present Arctic Ice Studio <development@arcticicestudio.com>
- * Copyright (c) 2017-present Sven Greb <development@svengreb.de>
+ * Copyright (C) 2017-present Arctic Ice Studio <development@arcticicestudio.com>
+ * Copyright (C) 2017-present Sven Greb <development@svengreb.de>
  *
  * Project:    Nord Visual Studio Code
  * Repository: https://github.com/arcticicestudio/nord-visual-studio-code
  * License:    MIT
- * References:
- *   https://prettier.io/docs/en/configuration.html
- *   https://prettier.io/docs/en/options.html
  */
 
 /**
@@ -15,16 +12,9 @@
  * @author Arctic Ice Studio <development@arcticicestudio.com>
  * @author Sven Greb <development@svengreb.de>
  * @see https://prettier.io/docs/en/configuration.html
+ * @see https://prettier.io/docs/en/options.html
  */
 
 module.exports = {
-  printWidth: 120,
-  overrides: [
-    {
-      files: ["*.json", "themes/**/*.json"],
-      options: {
-        parser: "json"
-      }
-    }
-  ]
+  printWidth: 120
 };
